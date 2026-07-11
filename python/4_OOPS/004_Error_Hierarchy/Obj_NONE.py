1f2832fa59fd6d0b4eb1192492a6191ee57e425e
Rajendra veerelli <84975266+rajendra5811@users.noreply.github.com>
2026-07-11 11:05:58 +0530

    Add files via upload

---
+++ python/4_OOPS/004_Error_Hierarchy/Obj_NONE.py
@@ -0,0 +1,15 @@
+ZeroDivisionError.mro()
+TypeError.mro()
+DeprecationWarning.mro()
+class Cat():
+    def __init__(self, name, age, isIndoor=True):
+        self.name = name
+        self.age = age
+        self.isIndoor = isIndoor
+
+    def speak(self):
+        print(f'{self.name} says, "purrrrrr"')
+
+herbert = Cat('herbert', 2)
+print(herbert.speak())
+print(herbert.name)
