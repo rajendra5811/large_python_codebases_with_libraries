3ee598ad294d8e5ada7181954f2629dedfa22bc4
Rajendra veerelli <84975266+rajendra5811@users.noreply.github.com>
2026-07-22 15:31:38 +0530

    Add files via upload

---
+++ python/6_practise/001_large_python_codebases_with_libraries/004_Python_in_system/004_Complex_Strategy/ImportEngine/Importer.py
@@ -0,0 +1,17 @@
+from typing import List
+
+from .ImportInterface import ImportInterface
+from .Cat import Cat
+from .DocxImporter import DocxImporter
+from .CSVImporter import CSVImporter
+from .PDFImporter import PDFImporter
+
+
+class Importer(ImportInterface):
+    importers = [DocxImporter, CSVImporter, PDFImporter]
+
+    @classmethod
+    def parse(cls, path: str) -> List[Cat]:
+        for importer in cls.importers:
+            if importer.can_ingest(path):
+                return importer.parse(path)
---
+++ python/6_practise/001_large_python_codebases_with_libraries/004_Python_in_system/004_Complex_Strategy/run.py
@@ -0,0 +1,5 @@
+from ImportEngine import  PDFImporter
+
+#print(DocxImporter.parse('./data/cats.docx'))
+#print(CSVImporter.parse('./data/cats.csv'))
+print(PDFImporter.parse('./data/cats.pdf'))
